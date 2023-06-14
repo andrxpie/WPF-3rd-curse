cd50d0c025662e2cdef61da37175aab045154a2c
andrxpie <venganpro12@gmail.com>
2023-06-14 20:52:23 +0300

    s

--- Table Shulte/ViewModels/ViewModel.cs
+++ Table Shulte/ViewModels/ViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Table_Shulte
 {
@@ -11,6 +12,17 @@ namespace Table_Shulte
         public ViewModel()
         {
 
+        }
+
+        private List<Grid> m_streams = new List<Grid>();
+        public List<Grid> Streams
+        {
+            get { return m_streams; }
+            set
+            {
+                m_streams = value;
+                OnPropertyChanged("Streams");
+            }
         }
     }
 }

62716b04ba26adece05137ce68560ed27c2d7fd1
Андрій Грицюк <venganpro12@gmail.com>
2023-06-14 17:02:49 +0300

    awd

---
+++ MVVM/ViewModels/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateOnly PubDate { get; set; }
+        public string Description { get; set; }
+    }
+}
--- MVVM/ViewModels/ViewModel.cs
+++ MVVM/ViewModels/ViewModel.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVVM.ViewModels
+namespace MVVM
 {
     public class ViewModel
     {
+        static string description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+        public List<Book> books = new List<Book>()
+        { 
+            new Book() { Name = "Hicksaw Ridge", Author = "Steve Man", PubDate = new DateOnly(2023, 6, 14), Description = ViewModel.description },
+            new Book() { Name = "The Great Gatsby", Author = "F. Scott Fitzgerald", PubDate = new DateOnly(1925, 4, 10), Description = ViewModel.description },
+            new Book() { Name = "Lonely worker", Author = "Alfrefo Gamb", PubDate = new DateOnly(988, 2, 23), Description = ViewModel.description }
+        };
+
+        public ViewModel() {}
+
+        public void NextBook(Book book)
+        {
+            
+        }
+
+        public void PrevBook()
+        {
+
+        }
     }
 }

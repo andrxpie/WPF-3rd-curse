11fc318f645f3c0cc6c33946b648c3376a63a8bc
Андрій Грицюк <venganpro12@gmail.com>
2023-06-19 15:03:35 +0300

    pre-final stage

---
+++ Table Shulte/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Table_Shulte.ViewModels;
+
+namespace Table_Shulte
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Restart(object sender, RoutedEventArgs e)
+        {
+            TableVM vm = (TableVM)DataContext;
+
+            if(vm != null)
+            {        
+                int diff = vm.Size;
+                DataContext = new TableVM(diff);
+            }
+            else MessageBox.Show("Game haven`t started");
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Difficulty(object sender, RoutedEventArgs e)
+        {           
+            for (int i = 0; i < 3; i++)
+            {
+                RadioButton RB = (RadioButton)difficultySTCK.Children[i];
+
+                if (RB.IsChecked == true)
+                {
+                    if((string)RB.Content == "Easy    (5 x 5)")
+                    {
+                        DataContext = new TableVM(5);
+                        return;
+                    }
+                    else if((string)RB.Content == "Medium  (7 x 7)")
+                    {
+                        DataContext = new TableVM(7);
+                        return;
+                    }
+                    else
+                    {
+                        DataContext = new TableVM(9);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void ClickCell(object sender, RoutedEventArgs e)
+        {
+            TableVM vm = (TableVM)DataContext;
+            Button btn = (Button)sender;
+
+            if((string)btn.Content == vm.CurrCell.ToString())
+            {
+                vm.CurrCell++;
+                btn.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ Table Shulte/ViewModels/TableVM.cs
@@ -0,0 +1,57 @@
+﻿using _09_shulte_table.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+
+namespace Table_Shulte.ViewModels
+{
+    public class TableVM
+    {
+        private IList<CellVM> cells = new List<CellVM>();
+        public IEnumerable<CellVM> Cells => cells;
+        public int FirstNumber => 1;
+        public int LastNumber => Size * Size - 1;
+        public int Size { get; }
+        public bool IsStarted { get; set; }
+        public int CurrCell { get; set; } = 1;
+
+        public TableVM(int size) 
+        {
+            Size = size;
+            IsStarted = true;
+
+            for (int i = FirstNumber; i <= LastNumber; ++i) cells.Add(new CellVM(i));
+            cells.Shuffle();
+
+            cells.Insert(cells.Count / 2, new CellVM());
+        }
+    }
+
+    public enum CellColor
+    {
+        White, Yellow, Green, Blue, Red, Purple
+    }
+
+    public class CellVM
+    {
+        private static Random rnd = new();
+
+        public int? Number { get; }
+        public CellColor Color { get; set; }
+        public bool IsEnable { get; set; } = false;
+
+        public CellVM(int? number = null)
+        {
+            Number = number;
+            Color = Number != null ? GetRandomColor() : CellColor.White;
+        }
+
+        private static CellColor GetRandomColor()
+        {
+            return (CellColor)rnd.Next(Enum.GetValues(typeof(CellColor)).Length);
+        }
+    }
+}
